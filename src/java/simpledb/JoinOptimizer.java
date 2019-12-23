f431b27da944a1039b1bf50338a5b268add56ffa
hearmingbear <hanjiaweiemail@icloud.com>
2019-12-23 09:58:08 +0800

    lab3exercise4

--- src/java/simpledb/JoinOptimizer.java
+++ src/java/simpledb/JoinOptimizer.java
@@ -16,7 +16,7 @@ public class JoinOptimizer {
 
     /**
      * Constructor
-     * 
+     *
      * @param p
      *            the logical plan being optimized
      * @param joins
@@ -34,7 +34,7 @@ public class JoinOptimizer {
      * inner/outer here -- because OpIterator's don't provide any cardinality
      * estimates, and stats only has information about the base tables. For this
      * reason, the plan1
-     * 
+     *
      * @param lj
      *            The join being considered
      * @param plan1
@@ -76,14 +76,14 @@ public class JoinOptimizer {
 
     /**
      * Estimate the cost of a join.
-     * 
+     *
      * The cost of the join should be calculated based on the join algorithm (or
      * algorithms) that you implemented for Lab 2. It should be a function of
      * the amount of data that must be read over the course of the query, as
      * well as the number of CPU opertions performed by your join. Assume that
      * the cost of a single predicate application is roughly 1.
-     * 
-     * 
+     *
+     *
      * @param j
      *            A LogicalJoinNode representing the join operation being
      *            performed.
@@ -111,14 +111,15 @@ public class JoinOptimizer {
             // HINT: You may need to use the variable "j" if you implemented
             // a join algorithm that's more complicated than a basic
             // nested-loops join.
-            return -1.0;
+            double cost = cost1 + card1 * cost2 + card1 * card2;
+            return cost;
         }
     }
 
     /**
      * Estimate the cardinality of a join. The cardinality of a join is the
      * number of tuples produced by the join.
-     * 
+     *
      * @param j
      *            A LogicalJoinNode representing the join operation being
      *            performed.
@@ -157,13 +158,25 @@ public class JoinOptimizer {
             Map<String, Integer> tableAliasToId) {
         int card = 1;
         // some code goes here
+        if(joinOp == Predicate.Op.EQUALS){
+            if(t1pkey){
+                card = card2;
+            }else if(t2pkey){
+                card = card1;
+            }else{
+                card = card1>card2 ?card1:card2;
+            }
+        }else{
+            double temp = 0.3 * card1 *card2;
+            card = (int)temp;
+        }
         return card <= 0 ? 1 : card;
     }
 
     /**
      * Helper method to enumerate all of the subsets of a given size of a
      * specified vector.
-     * 
+     *
      * @param v
      *            The vector whose subsets are desired
      * @param size
@@ -196,7 +209,7 @@ public class JoinOptimizer {
     /**
      * Compute a logical, reasonably efficient join on the specified tables. See
      * PS4 for hints on how this should be implemented.
-     * 
+     *
      * @param stats
      *            Statistics for each table involved in the join, referenced by
      *            base table names, not alias
@@ -221,7 +234,38 @@ public class JoinOptimizer {
 
         // some code goes here
         //Replace the following
-        return joins;
+        PlanCache planCache = new PlanCache();
+        Set<Set<LogicalJoinNode>> nodeSets  = null;
+        for(int i=0;i<joins.size()+1;++i){
+            nodeSets = enumerateSubsets(joins,i);
+            for(Set<LogicalJoinNode> nodeSet:nodeSets){
+                double costSoFar = Double.MAX_VALUE;
+                for(LogicalJoinNode node:nodeSet){
+                    // computeCostAndCardOfSubplan(HashMap<String, TableStats> stats,
+                    //                                                HashMap<String, Double> filterSelectivities,
+                    //                                                LogicalJoinNode joinToRemove,
+                    //                                                Set<LogicalJoinNode> joinSet,
+                    //                                                double bestCostSoFar,
+                    //                                                PlanCache pc)
+                    CostCard costCard = computeCostAndCardOfSubplan(stats,filterSelectivities,
+                            node,nodeSet,costSoFar,planCache);
+                    if(costCard == null) continue;
+                    if(costCard.cost < costSoFar){
+                        costSoFar = costCard.cost;
+                        //void addPlan(Set<LogicalJoinNode> s, double cost, int card, Vector<LogicalJoinNode> order)
+                        planCache.addPlan(nodeSet,costSoFar,costCard.card,costCard.plan);
+                    }
+                }
+            }
+        }
+        Vector<LogicalJoinNode> res = null;
+        for (Set<LogicalJoinNode> nodeSet : nodeSets)
+            res = planCache.getOrder(nodeSet);
+
+        if (explain)
+            printJoins(res, planCache, stats, filterSelectivities);
+
+        return res;
     }
 
     // ===================== Private Methods =================================
@@ -231,7 +275,7 @@ public class JoinOptimizer {
      * joinToRemove to joinSet (joinSet should contain joinToRemove), given that
      * all of the subsets of size joinSet.size() - 1 have already been computed
      * and stored in PlanCache pc.
-     * 
+     *
      * @param stats
      *            table stats for all of the tables, referenced by table names
      *            rather than alias (see {@link #orderJoins})
@@ -391,7 +435,7 @@ public class JoinOptimizer {
     /**
      * Return true if field is a primary key of the specified table, false
      * otherwise
-     * 
+     *
      * @param tableAlias
      *            The alias of the table in the query
      * @param field
@@ -422,7 +466,7 @@ public class JoinOptimizer {
      * Helper function to display a Swing window with a tree representation of
      * the specified list of joins. See {@link #orderJoins}, which may want to
      * call this when the analyze flag is true.
-     * 
+     *
      * @param js
      *            the join plan to visualize
      * @param pc
